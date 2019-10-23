55c4720c294e58e90dc9b8b1955d2a924f49e817
Mason Corbett <corbettmc@mac.com>
2019-10-22 20:23:45 -0400

    changed stuff

---
+++ build/precache-manifest.54a13b6dd646d42894e11f9f907fda74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719b924a801a034778e2ff935fa1bbf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf65d30f09c6ec223187",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d11ac31b71c336cd26b3",
+    "url": "/static/js/2.c58a6dcd.chunk.js"
+  },
+  {
+    "revision": "cf65d30f09c6ec223187",
+    "url": "/static/js/main.bf3a0d9e.chunk.js"
+  },
+  {
+    "revision": "e2f7039fe862ab1337ee",
+    "url": "/static/js/runtime-main.921a4f5a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
